120a1707c981104599ab7e8aafd4d2e5c678e469
macalistervadim <macalistervadim@yandex.ru>
2023-09-15 11:40:43 +0600

    add rabbitmq and celery

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from .tasks import order_created
 
 def order_create(request):
     cart = Cart(request)
@@ -17,6 +18,7 @@ def order_create(request):
                                     quantity=item['quantity'])
             # очистить корзину
             cart.clear()
+            order_created.delay(order.id)
             return render(request,
             'orders/order/created.html',
                     {'order': order})
