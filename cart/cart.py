7218f954fec4580b2b3d3c6da051d64060ce8b04
macalistervadim <macalistervadim@yandex.ru>
2023-09-24 12:32:19 +0600

    Add coupons

--- cart/cart.py
+++ cart/cart.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.conf import settings
 
 from shop.models import Product
+from coupons.models import Coupon
 
 class Cart:
     def __init__(self, request):
@@ -14,6 +15,8 @@ class Cart:
             # сохранить пустую корзину в сеансе
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        # сохранить текущий примененный купон
+        self.coupon_id = self.session.get('coupon_id')
 
     def add(self, product, quantity=1, override_quantity=False):
         """
@@ -73,3 +76,21 @@ class Cart:
         # удалить корзину из сеанса
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) \
+                * self.get_total_price()
+        return Decimal(0)
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
